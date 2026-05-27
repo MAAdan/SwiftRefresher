f30cc0baa31c74f7af29c43711abbab535c9c553
Miguel Adan <adan.miguel@gmail.com>
2026-05-27 22:50:51 +0200

    Add Generics playground

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+// Generics allow us to tell a function or type, “I know Swift is a type-safe language, but I do not know the type that will be needed yet. I will give you a placeholder for now and will let you know what type to enforce later.”
+
+//Example of functions that swap the values of two variables
+func swapInts(a: inout Int,b: inout Int) {
+    let tmp = a
+    a = b
+    b = tmp
+}
+
+func swapDoubles(a: inout Double,b: inout Double) {
+    let tmp = a
+    a = b
+    b = tmp
+}
+
+func swapStrings(a: inout String, b: inout String) {
+    let tmp = a
+    a = b
+    b = tmp
+}
+
+var a = 5
+var b = 10
+swapInts(a: &a, b: &b)
+
+print("a:\(a) b:\(b)")
+
+//The & symbol is used in conjunction with the inout parameter to pass the values as reference
+
+//Defining a generic function
+func swapGeneric<T>(a: inout T, b: inout T) {
+    let tmp = a
+    a = b
+    b = tmp
+}
+
+swapGeneric(a: &a, b: &b)
+print("a:\(a) b:\(b)")
+
+var c = "My String 1"
+var d = "My String 2"
+swapGeneric(a: &c, b: &d)
+
+print("c:\(c) d:\(d)")
+
+//Type constraints
+//These specify that a generic type must inherit from a specific class or conform to a particular protocol. This allows us to use the methods or properties defined by the parent class or protocol within the generic function.
+
+func testGenericComparable<T: Comparable>(a: T, b: T) -> Bool {
+    return a == b
+}
+
+class MyClass { }
+protocol MyProtocol { }
+
+//In this function, the type defined by the T placeholder must inherit from the MyClass class, and the type defined by the E placeholder must conform to the MyProtocol protocol
+func testFunction<T: MyClass, E: MyProtocol>(a: T, b: E) {
+    //Statements
+}
+
